84a7114410b1fd471510c6c30bf2730abef477a5
Reshad Noorzay <rnoorzay@gmail.com>
2013-04-04 15:48:30 -0700

    included Object.create function, inheritPrototype function and Question function

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+// Douglas C's Object.create()
+if (typeof Object.create !== 'function') {
+	Object.create = function (o) {
+		function F() {}
+		F.prototype = o;
+		return new F();
+	};
+}
+// Parasitic Combination Inheritance
+function inheritPrototype(childObject, parentObject) {
+	var copyOfParent = Object.create(parentObject.prototype);
+	copyOfParent.constructor = childObject;
+	childObject.prototype = copyOfParent;
+}
+
 // Quiz application in JS OOP
 
 function User (theName, theEmail) {
@@ -22,7 +37,65 @@ User.prototype = {
 	}
 };
 
-// a User
+function Question (theQuestion, theChoices, theCorrectAnswer) {
+	this.question = theQuestion;
+	this.choices = theChoices;
+	this.correctAnswer = theCorrectAnswer;
+	this.userAnswer = "";
+
+	var newDate = new Date(),
+	QUIZ_CREATED_DATE = newDate.toLocaleDateString();
+	
+	this.getQuizDate = function () {
+		return QUIZ_CREATED_DATE;
+	};
+
+	console.log("Quiz Created On: " + this.getQuizDate());
+}
+
+Question.prototype.getCorrectAnswer = function () {
+	return this.correctAnswer;
+};
+Question.prototype.getUserAnswer = function () {
+	return this.userAnswer;
+};
+Question.prototype.displayQuestion = function () {
+	var questionToDisplay = "<div class='question'>" + this.question + "</div><ul>";
+	choiceCounter = 0;
+	this.choices.forEach(function (eachChoice) {
+		questionToDisplay += '<li><input type="radio" name="choice" value="' + choiceCounter + '">' + eachChoice + '</li>';
+		choiceCounter++;
+	});
+	questionToDisplay += "</ul>";
+	console.log(questionToDisplay);
+};
+
+function MultipleChoiceQuestion(theQuestion, theChoices, theCorrectAnswer) {
+	Question.call(this, theQuestion, theChoices, theCorrectAnswer);
+};
+
+inheritPrototype(MultipleChoiceQuestion, Question);
+
+function DragDropQuestion(theQuestion, theChoices, theCorrectAnswer) {
+	Question.call(this, theQuestion, theChoices, theCorrectAnswer);
+}
+
+inheritPrototype(DragDropQuestion, Question);
+
+DragDropQuestion.prototype.displayQuestion = function () {
+	console.log(this.question);
+}
+
+var allQuestions = [
+new MultipleChoiceQuestion("Who is the President of the U.S.?", ["Obama", "Brown", "Martin", "Clinton"], 0),
+new MultipleChoiceQuestion("What is the Capital of California?", ["Texas", "Sacramento", "Los Angeles", "San Francisco"], 1),
+new DragDropQuestion("Drag the capitol of the United States to the map: ", ["Washington, DC", "London", "Rio de Janeiro", "Kabul"], 0)
+];
+
+allQuestions.forEach(function(eachQuestion) {
+	eachQuestion.displayQuestion();
+});
+
 firstUser = new User("Reshad", "reshad@example.com");
 firstUser.changeEmail("reshadn@example.com");
 firstUser.saveScore(10);
